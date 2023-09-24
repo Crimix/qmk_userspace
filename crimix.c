74c470535579f1709c2bed3db40223adb4a4a8b0
crimix <christofferm@live.dk>
2023-09-24 15:11:54 +0200

    Extra conditional

--- crimix.c
+++ crimix.c
@@ -8,12 +8,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_leader(keycode, record)) { return false; }
 
     switch (keycode) {
+#ifdef CAPS_WORD_ENABLE
         case CAPS_WORD:
             if (record->event.pressed) {
                 toggle_caps_word_mode(CAPS_WORD_MODE_DEFAULT);
                 return false;
             }
             break;
+#endif // CAPS_WORD_ENABLE
     }
     return true;
 }
