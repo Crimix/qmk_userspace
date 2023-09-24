b820d80bef856b87deb9bef2f8b2e3fd2aec4e7d
crimix <christofferm@live.dk>
2023-09-24 15:07:40 +0200

    Try again

--- crimix.c
+++ crimix.c
@@ -1,1 +1,19 @@
 #include QMK_KEYBOARD_H
+
+#ifdef CAPS_WORD_ENABLE
+    #include "common/features/caps_word.h"
+#endif // CAPS_WORD_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_leader(keycode, record)) { return false; }
+
+    switch (keycode) {
+        case CAPS_WORD:
+            if (record->event.pressed) {
+                toggle_caps_word_mode(CAPS_WORD_MODE_DEFAULT);
+                return false;
+            }
+            break;
+    }
+    return true;
+}
--- rules.mk
+++ rules.mk
@@ -1,1 +1,4 @@
 SRC += crimix.c
+
+CAPS_WORD_ENABLE    = yes
+  SRC += common/features/caps_word.c
