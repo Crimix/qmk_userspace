a7bcbe089692464fef0d44fea1175fccfd4630be
crimix <christofferm@live.dk>
2023-09-24 15:37:47 +0200

    Try again

--- crimix.c
+++ crimix.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #ifdef CAPS_WORD_ENABLE
-    #include "common/features/caps_word.h"
+    #include "common/features/caps_word/caps_word.h"
 #endif // CAPS_WORD_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
--- rules.mk
+++ rules.mk
@@ -1,4 +1,4 @@
 SRC += crimix.c
 
 CAPS_WORD_ENABLE    = yes
-  SRC += common/features/caps_word.c
+  SRC += common/features/caps_word/caps_word.c
