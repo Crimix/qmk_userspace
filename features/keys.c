3c1fe463638a6d15654fa5f6081298178bff47ee
crimix <christofferm@live.dk>
2023-10-28 16:15:55 +0200

    Try this

--- features/keys.c
+++ features/keys.c
@@ -1,3 +1,12 @@
 #include QMK_KEYBOARD_H
 #include "keys.h"
-#include "keymap.h"
+
+enum custom_keycodes {
+    KC_CW_NORMAL = COMMON_START,
+    KC_CW_COC,
+    KC_CW_CAC,
+    KC_CW_SNC,
+    KC_SC_INTELLIJ_RENAME,
+    KC_SC_INTELLIJ_GENERATE,
+    SAFE_RANGE_AFTER_COMMON
+};
