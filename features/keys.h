6b857683ab1ce62f6fd5604734436886e5170dc2
crimix <christofferm@live.dk>
2023-10-28 12:13:38 +0200

    Make ready for the future

--- features/keys.h
+++ features/keys.h
@@ -1,7 +1,13 @@
+#ifdef KEYBOARD_keychron
+#    define COMMON_START USER_START
+#else
+#    define COMMON_START SAFE_RANGE
+#endif
+
 enum {
-    KC_CW_NORMAL = NEW_SAFE_RANGE,
+    KC_CW_NORMAL = COMMON_START,
     KC_CW_COC,
     KC_CW_CAC,
     KC_CW_SNC,
-	NEW_SAFE_RANGE_AFTER_COMMON
+	SAFE_RANGE_AFTER_COMMON
 };
