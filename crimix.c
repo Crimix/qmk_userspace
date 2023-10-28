c972f069c4118686e54f2397d4eeeb7274c8542c
crimix <christofferm@live.dk>
2023-10-28 12:39:47 +0200

    More color work

--- crimix.c
+++ crimix.c
@@ -41,19 +41,30 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (get_highest_layer(layer_state) == 4) {
-        uint8_t layer = get_highest_layer(layer_state);
-
+    uint8_t layer = get_highest_layer(layer_state);
+    if (layer == 4 || layer == 3) {
         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
 
-                if (index >= led_min && index < led_max && index != NO_LED &&
-                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_BLUE);
+                if (index >= led_min && index < led_max && index != NO_LED) {
+                    uint16_t keycode = keymap_key_to_keycode(layer, (keypos_t){col,row});
+                    if (keycode > KC_TRNS) {
+                        if (layer == 3) {
+                            rgb_matrix_set_color(index, RGB_ORANGE);
+                        } else if (layer == 4) {
+                            // if (keycode == ) {
+                            //     rgb_matrix_set_color(index, RGB_BLUE);
+                            // } else {
+                            rgb_matrix_set_color(index, RGB_BLUE);
+                            // }
+                        }
+                    }
                 }
             }
         }
     }
     return false;
 }
+
+
