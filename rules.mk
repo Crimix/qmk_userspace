e6bcb4ec6bcec256a2bee2e0efb5d1794bac68f4
crimix <christofferm@live.dk>
2023-10-28 15:37:56 +0200

    Maybe this will work

--- rules.mk
+++ rules.mk
@@ -4,6 +4,7 @@ VIA_ENABLE = yes
 BOOTMAGIC_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 
+  SRC += common/features/keys.c
 CAPS_WORD_ENABLE    = yes
   SRC += common/features/caps_word/caps_word.c
   SRC += common/features/shortcuts/shortcuts.c
