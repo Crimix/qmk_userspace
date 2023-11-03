99dd2822de6d8e16e8724800bfa7e8801c5a1666
crimix <christofferm@live.dk>
2023-11-04 00:53:40 +0100

    Fix missing rename

--- rgb_layers/rgb_layers.c
+++ rgb_layers/rgb_layers.c
@@ -7,7 +7,7 @@ layer_state_t layer_state_set_keymap(layer_state_t state) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     uint8_t layer = get_highest_layer(state);
-    if (layer == SPECIAL || layer == WIN_FN) {
+    if (layer == COMMON_SPECIAL || layer == COMMON_WIN_FN) {
         rgb_matrix_enable_noeeprom();
     } else {
         rgb_matrix_reload_from_eeprom();
@@ -23,7 +23,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
 bool rgb_matrix_indicators_advanced_keymap(uint8_t led_min, uint8_t led_max) {
     uint8_t layer = get_highest_layer(layer_state);
-    if (layer == SPECIAL || layer == WIN_FN) {
+    if (layer == COMMON_SPECIAL || layer == COMMON_WIN_FN) {
         for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
@@ -31,9 +31,9 @@ bool rgb_matrix_indicators_advanced_keymap(uint8_t led_min, uint8_t led_max) {
                 if (index >= led_min && index < led_max && index != NO_LED) {
                     uint16_t keycode = keymap_key_to_keycode(layer, (keypos_t){col,row});
                     if (keycode > KC_TRNS) {
-                        if (layer == WIN_FN) {
+                        if (layer == COMMON_WIN_FN) {
                             rgb_matrix_set_color(index, RGB_ORANGE);
-                        } else if (layer == SPECIAL) {
+                        } else if (layer == COMMON_SPECIAL) {
                             if (is_intellij_key(keycode)) {
                                 rgb_matrix_set_color(index, RGB_PURPLE);
                             } else {
