1eeddadfcda21e4c6e7bf43b9ed8fd5df6e603a9
crimix <christofferm@live.dk>
2023-11-04 01:18:19 +0100

    Switch order

--- config.h
+++ config.h
@@ -1,8 +1,7 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define FIRST_NAME "Christoffer"
+#define LAYER_START 3 //WIN_FN
 
 #ifndef __ASSEMBLER__ // Guard against use with non-C files
 #    include "wrappers.h"
 #endif
-
-#define FIRST_NAME "Christoffer"
-#define LAYER_START 3 //WIN_FN
