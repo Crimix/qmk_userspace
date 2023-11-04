7545a6b33f1f52a764de7e8cedbf4e9af81715ef
crimix <christofferm@live.dk>
2023-11-04 12:55:19 +0100

    Implement battery rgb

--- crimix.c
+++ crimix.c
@@ -18,6 +18,15 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool pre_rgb_layer_highlight(uint8_t led_min, uint8_t led_max) {
+    if (disable_rgb_layers) {
+        if (battery_get_percentage() >= 20 && battery_get_percentage() < 50) {
+            rgb_matrix_set_color_all(RGB_YELLOW)
+        } else if (battery_get_percentage() < 20) {
+            rgb_matrix_set_color_all(RGB_RED)
+        } else {
+            rgb_matrix_set_color_all(RGB_GREEN)
+        }
+    }
     return disable_rgb_layers;
 }
 
