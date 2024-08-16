ae0624672406c5a35b31f667591b011307f2c9f5
crimix <christofferm@live.dk>
2024-08-16 17:25:52 +0200

    Fix

--- ansi_mode/ansi_mode.h
+++ ansi_mode/ansi_mode.h
@@ -11,6 +11,6 @@ typedef enum {
 
 ansi_mode_state_t g_ansi_mode_state;
 
-bool is_ansi_mode(void)
+bool is_ansi_mode(void);
 void toggle_ansi_mode(ansi_mode_state_t state);
 bool process_record_ansi_mode(uint16_t keycode, keyrecord_t *record);
