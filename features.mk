f1f7f0ea0f977de6141c40a651f69d2ce435fee8
crimix <christofferm@live.dk>
2025-08-20 10:02:04 +0200

    Try something

--- features.mk
+++ features.mk
@@ -6,6 +6,7 @@ endif
 SRC += ansi_mode/ansi_mode.c
 SRC += shortcuts/shortcuts.c
 SRC += special_layer/special_layer.c
+INTROSPECTION_KEYMAP_C += special_layer/special_layer.c
 ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
 SRC += rgb_layers/rgb_layers.c
 endif
