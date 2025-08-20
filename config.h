3fae3308170edb110eb3f36d21e20051df883119
crimix <christofferm@live.dk>
2025-08-20 11:24:14 +0200

    Set correct index

--- config.h
+++ config.h
@@ -7,6 +7,8 @@
 #define RGB_MATRIX_DEFAULT_HUE 0
 #define RGB_MATRIX_DEFAULT_SAT 0
 
+#define ANSI_MODE_INDEX 47
+
 #ifndef __ASSEMBLER__ // Guard against use with non-C files
 #    include "wrappers.h"
 #endif
